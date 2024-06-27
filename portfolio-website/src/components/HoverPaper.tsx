0fdba92ed985e1f43311f03ad104c1e62be7cc57
Ruthesh-Thavamani <truthesh@gmail.com>
2024-06-27 19:50:55 -0400

    added pop out transition

--- portfolio-website/src/components/HoverPaper.tsx
+++ portfolio-website/src/components/HoverPaper.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 
 function HoverPaper({props}) {
     const [borderColor, setBorderColor] = useState(alpha('#FFF', 0.2));
+    const [degree, setDegree] = useState(0);
 
     return (
         <Paper
@@ -18,10 +19,13 @@ function HoverPaper({props}) {
             borderColor: borderColor,
             boxShadow: borderColor === '#90CAF9' ? '0 0 13px #90CAF9' : 'none',
             background: `linear-gradient(${alpha('#033363', 0.2)}, ${alpha('#021F3B', 0.2)})`,
-            transition: 'border-color 0.23s ease, box-shadow 0.5s ease',
+            transition: 'border-color 0.23s ease, box-shadow 0.5s ease, transform 0.3s ease', 
+            '&:hover': {
+                transform: 'scale(1.02) rotate(' + degree + 'deg)',
+            },
         }}
-        onMouseEnter={() => setBorderColor('#90CAF9')}
-        onMouseLeave={() => setBorderColor(alpha('#FFF', 0.2))}
+        onMouseEnter={() => {setBorderColor('#90CAF9'); setDegree(Math.random() * 4 - 2);}}
+        onMouseLeave={() => {setBorderColor(alpha('#FFF', 0.2)); setDegree(0);}}
         >
         {props}
         </Paper>
